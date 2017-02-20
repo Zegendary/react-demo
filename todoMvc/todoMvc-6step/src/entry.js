de241cf4d3de52447c1e29aa33b6785f9aa6470d
Zegendary <zxw1992513@qq.com>
2017-02-20 14:22:28 +0800

    完成todomvc功能

---
+++ todoMvc/todoMvc-6step/src/entry.js
@@ -0,0 +1,6 @@
+'use strict';
+// require('./components/app')
+// require('./styles/main.styl')
+
+import './components/app'  //es6写法
+import './styles/main.styl'   //es6写法
---
+++ todoMvc/todoMvc-6step/webpack.config.js
@@ -0,0 +1,20 @@
+/**
+ * Created by zhangxinwang on 16/02/2017.
+ */
+module.exports = {
+  entry: [
+    "./src/entry.js" //入口文件
+  ],
+  output: {
+    path: './out/',
+    filename: "bundle.js" //输出文件
+  },
+  module: {
+    loaders: [
+      { test: /\.js[x]?$/, loader: "babel-loader?presets[]=es2015&presets[]=react", include: /src/},//支持es6
+      { test: /\.css$/, loader: "style-loader!css-loader"},
+      { test: /\.styl$/, loader: "style-loader!css-loader!stylus-loader"},//支持stylus
+      { test: /\.(png|jpg)$/, loader: 'url?limit=8192'}
+    ]
+  }
+};
